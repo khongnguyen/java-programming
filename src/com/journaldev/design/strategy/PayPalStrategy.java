d98952555b8eec2114438d5ae01ea1a01fac9b6a
hieu2102 <gobanme.pierro@gmail.com>
2019-12-24 10:38:54 +0700

    hieund_day1

---
+++ src/com/journaldev/design/strategy/PayPalStrategy.java
@@ -0,0 +1,15 @@
+package com.journaldev.design.strategy;
+
+public class PayPalStrategy implements PaymentStrategy {
+	private String emailId;
+	private String password;
+	public PayPalStrategy(String emailId, String password) {
+		this.emailId = emailId;
+		this.password = password;
+	}
+
+	@Override
+	public void pay(int price) {
+	}
+
+}
