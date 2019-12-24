0ff5a249b6d3b3b7cd6555720103ba5fdd240cb3
tuan-hv <anwabong@gmail.com>
2019-12-24 15:50:17 +0700

    cy

---
+++ src/main/java/exceptions/DuplicateCodeException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class DuplicateCodeException extends Exception {
+    public DuplicateCodeException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/exceptions/TooOldException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class TooOldException extends AgeException {
+    public TooOldException(String message) {
+        super(message);
+    }
+}
