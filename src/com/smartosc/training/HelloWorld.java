0ed510f1f1b006577349fe972d6a0369638b415c
zedlight168 <zedlight1681995@gmail.com>
2019-12-24 14:22:39 +0700

    test commit

--- src/com/smartosc/training/HelloWorld.java
+++ src/com/smartosc/training/HelloWorld.java
@@ -3,7 +3,7 @@ package com.smartosc.training;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World copy");
 	}
 
 }
